9f97ce242e29fac7c2690caae848289d924dc232
Reidi Zhupa <35573511+reidizhupa@users.noreply.github.com>
2026-06-25 11:57:10 +0900

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,33 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    reactStrictMode: true,
+    trailingSlash: true,
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "sankogakuen-hp.vercel.app",
+            },
+        ],
+    },
+    async headers() {
+        return [
+            {
+                source: "/(.*)",
+                headers: [
+                    {
+                        key: "X-Frame-Options",
+                        value: "DENY",
+                    },
+                    {
+                        key: "X-Content-Type-Options",
+                        value: "nosniff",
+                    },
+                ],
+            },
+        ];
+    },
 };
 
 export default nextConfig;
